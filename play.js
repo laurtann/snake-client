cd589a524ad44492c196fec54cbf89ab9b5419b7
Laura Tannahill <lntannahill@gmail.com>
2020-11-04 18:22:03 +0000

    Created client side file and refactored play.js

--- play.js
+++ play.js
@@ -1,19 +1,5 @@
 const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('connect', () => console.log("Connection successfully established to game server"))
-
-  return conn;
-}
-
+const connect = require('./client');
 console.log('Connecting ...');
 connect();
