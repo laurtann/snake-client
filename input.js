8e6ac40e97ff8d19e869dd4df19de27965af3c38
Laura Tannahill <lntannahill@gmail.com>
2020-11-04 23:38:17 +0000

    Implemented WASD commands

--- input.js
+++ input.js
@@ -1,16 +1,36 @@
-const setupInput = function() {
+const { connect } = require('./play');
+
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', (key) => handleUserInput(key))
+  stdin.on('data', handleUserInput);
   return stdin;
 };
 
 const handleUserInput = function(key) {
-  // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
+  }
+  //key code for w '87'
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  // a '65'
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  // s
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  // d '68'
+  if (key === 'd') {
+    connection.write("Move: right");;
   }
 };
 
--- play.js
+++ play.js
@@ -2,6 +2,6 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
+// this is returning the conn obj
 
-setupInput();
+setupInput(connect());
